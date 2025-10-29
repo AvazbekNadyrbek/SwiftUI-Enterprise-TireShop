16d43d7f520359dcda5533505a7dabfccbf80369
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-10-29 08:14:59 +0600

    Initial Commit

---
+++ GlobalErrorHandling/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GlobalErrorHandling
+//
+//  Created by Авазбек Надырбек уулу on 29.10.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GlobalErrorHandling/GlobalErrorHandlingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalErrorHandlingApp.swift
+//  GlobalErrorHandling
+//
+//  Created by Авазбек Надырбек уулу on 29.10.25.
+//
+
+import SwiftUI
+
+@main
+struct GlobalErrorHandlingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
